46d407db00f92381051506b06278c082924ba821
Muhsini Mohamed <muhcnlego@gmail.com>
2024-08-25 21:06:16 +0200

    added welcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+*/
+import WelcomeMessage from './components/WelcomeMessage';
 
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+            {/* Other components or content */}
+        </div>
+    );
+}
+
+export default App;
+
+/*
 function App() {
   const [count, setCount] = useState(0)
 
@@ -33,3 +47,4 @@ function App() {
 }
 
 export default App
+*/
