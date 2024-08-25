7746f4b1a9dab9efc1de61725522d3cad13cb90f
Muhsini Mohamed <muhcnlego@gmail.com>
2024-08-26 01:36:36 +0200

    added my-company

--- my-company/src/App.js
+++ my-company/src/App.js
@@ -1,4 +1,3 @@
-/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,37 +19,6 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
-*/
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
-          <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-          <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-          <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
-          <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
   );
 }
 
