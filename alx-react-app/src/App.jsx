05ef7a7bf9f679ea45ea7d0e6429f8e692eb5f16
Muhsini Mohamed <muhcnlego@gmail.com>
2024-08-25 21:54:50 +0200

    initialze userportfolio

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -59,7 +59,12 @@ export default App;
 */
 
 
-
+/*
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+*/
+/*
 function App() {
   return (
       <div>
@@ -71,5 +76,16 @@ function App() {
 }
 
 export default App;
+*/
 
+import UserProfile from './components/UserProfile';
 
+function App() {
+  return (
+      <div>
+          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+  );
+}
+
+export default App;
