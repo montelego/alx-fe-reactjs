879fe3a8e1cf64b866da964f9cec6ad36be7891f
Muhsini Mohamed <muhcnlego@gmail.com>
2024-08-28 23:28:23 +0200

    added inline style in navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/*
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -7,6 +8,28 @@ function Navbar() {
       <Link to="/about" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Services</Link>
       <Link to="/contact" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
+*/
+
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{
+      padding: '10px',
+      backgroundColor: '#333',
+      color: '#fff',
+      display: 'flex',
+      justifyContent: 'space-around'
+    }}>
+      <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
