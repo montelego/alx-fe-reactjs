22ac41b62d7188c0d7261fdfee76b416822d554d
Muhsini Mohamed <muhcnlego@gmail.com>
2024-08-31 20:12:44 +0200

    moved the todo.test to the store folder

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 // src/__tests__/TodoList.test.js
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from './TodoList';
+import TodoList from '../components/TodoList';
 
 test('renders the initial todos', () => {
   render(<TodoList />);
