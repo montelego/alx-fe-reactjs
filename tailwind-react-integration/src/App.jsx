76dcdd68863fc25e29883fc58c0038d4892b81f8
Muhsini Mohamed <muhcnlego@gmail.com>
2024-09-03 21:19:43 +0200

    updated app.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,17 @@ function App() {
 }
 
 export default App
+*/
+
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
