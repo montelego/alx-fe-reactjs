186b32592589296a47271a016f0dff70d623bbab
Muhsini Mohamed <muhcnlego@gmail.com>
2024-09-04 17:15:27 +0200

    nade another changes in the userprofile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -36,9 +36,28 @@ function UserProfile() {
 export default UserProfile;
 */
 
+/*
 function UserProfile() {
   return (
     <div className="max-w-xs sm:max-w-sm md:max-w-lg mx-auto bg-white rounded-lg shadow-lg p-4 sm:p-4 md:p-8">
+      <img className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto" src="profile.jpg" alt="User Profile" />
+      <h2 className="text-lg sm:text-lg md:text-xl font-semibold">
+        User Name
+      </h2>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">
+        User bio or description goes here.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
+*/
+
+
+function UserProfile() {
+  return (
+    <div className="max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg mx-auto bg-white rounded-lg shadow-lg p-4 sm:p-6 md:p-8">
       <img className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto" src="profile.jpg" alt="User Profile" />
       <h2 className="text-lg sm:text-lg md:text-xl font-semibold">
         User Name
